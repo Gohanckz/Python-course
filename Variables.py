4c20d94b8094144885f71ed38ddc1d0824d88a15
taekcoder <ing.ivan.rodrigo@gmail.com>
2018-01-21 17:06:26 -0300

    suma de dos variables y convercion de int a texto

---
+++ Variables.py
@@ -0,0 +1,9 @@
+print("compra de dos productos")
+nombre1 = input("Ingrese el nombre del primer producto : ")
+precio1 = int(input("Ingrese el precio : "))
+nombre2 = input("Ingrese el nombre del segundo producto : ")
+precio2 = int(input("Ingrese el precio : "))
+
+total_compra = precio1 + precio2
+
+print(" el total de la compra es de  : "+ str(total_compra))
