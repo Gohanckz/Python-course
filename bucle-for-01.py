2548c834c02cf3d18e079d525d9df5bd6518fd3f
taekcoder <ing.ivan.rodrigo@gmail.com>
2018-01-21 22:08:20 -0300

    Bucle for whit range / ejercicio 01

---
+++ bucle-for-01.py
@@ -0,0 +1,7 @@
+#Realizar un programa que imprima en pantalla los numeros del 20 al 30.
+
+for i in range(20,31):
+    print(i)
+#La funcion range puede tener dos parámetros, el primero indica el valor inicial
+#El segundo parametro indica el valor final - 1
+     
