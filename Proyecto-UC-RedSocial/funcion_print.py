8efc2d9912126c7dda640d8dc2e40951f9392088
taekcoder <ing.ivan.rodrigo@gmail.com>
2018-01-26 02:54:40 -0300

    Funcion print y algunas de sus utilidades

---
+++ Proyecto-UC-RedSocial/funcion_print.py
@@ -0,0 +1,21 @@
+#Para escribir un texto y mostrarlo ne pantalla utilizamos la funcion 'print'
+
+print("Hello world")
+
+#podemos escribir mas de una palabra separandolas por comas ',' y automaticamente el propraga nos asignará un espacio
+
+print("Esto","es",1,"prueba")
+
+#tambien podemos hacer que una linea sea continua utilizando 'end=" "'
+
+edad=23
+nombre = "ivan"
+
+print("Hola soy", nombre ," y tengo ", end="")
+print(edad)
+print("cumplidos")
+
+#Otra forma de separar un print es utilizando la expresion 'sep='
+#Con esto elegimos lo que queremos que separe en vez de un espacio.
+
+print("El","tesoro","esta",sep="...")
