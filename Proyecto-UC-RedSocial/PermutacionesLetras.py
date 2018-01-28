df2fab8329860a6542249f28c9b58cbc2e1eae1c
taekcoder <ing.ivan.rodrigo@gmail.com>
2018-01-28 14:05:40 -0300

    Programa para permutar posiciones de letras

---
+++ Proyecto-UC-RedSocial/PermutacionesLetras.py
@@ -0,0 +1,7 @@
+from itertools import product,permutations
+
+
+letters = ("P","B","J","E","B","K","T","E","O","P","V","R")
+#print(list(product(letters,range(5))))
+print(list(permutations(letters)))
+palabra = letters
