d228aae7b8083a00ba2e19128c9d0bc90895b7ae
taekcoder <ing.ivan.rodrigo@gmail.com>
2018-01-28 14:06:50 -0300

    Funciones001 - llamar y mostrar resultado de una funcion

---
+++ Proyecto-UC-RedSocial/Funciones001.py
@@ -0,0 +1,10 @@
+
+#Definimos la funcion con la operacion que queramo que se realice
+def calculo(numero):
+    resultado = (numero-3)**3
+    return resultado
+
+#Llamamos la funcion por su nombre y le asignamos el valor que queramos que este lleve dentro.
+print(calculo(8))
+
+print(calculo(5))
