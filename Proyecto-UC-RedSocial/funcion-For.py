56612461aea21034643eec8618e896c4b86d8d37
taekcoder <ing.ivan.rodrigo@gmail.com>
2018-01-27 21:49:32 -0300

    Agregamos comentarios y funciones de range para ciclo FOR

--- Proyecto-UC-RedSocial/funcion-For.py
+++ Proyecto-UC-RedSocial/funcion-For.py
@@ -4,3 +4,14 @@
 print("F°   C°")
 for temp in range(21):
     print(temp,"    ",int((temp-32)*5/9))
+
+
+"""
+    Se puede utilizar la funcion range de diferentes formas.
+    
+    por ejemplo: range(5,10) -> comenzará desde 5 hasta llegar a 9 (10-1 = 9)
+
+    sin embargo, tambien podemos asignar el aumento que queramos que posea 
+    
+    por ejemplo: range(5,10,2) -> comenzará desde 5 hasta llegar a 9 pero el aumento es de 2 en 2
+"""
