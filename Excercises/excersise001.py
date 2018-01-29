b9af86e482e63a8b96594828f14425a0680871b4
taekcoder <ing.ivan.rodrigo@gmail.com>
2018-01-28 21:37:01 -0300

    Excersise001 - Mayor de dos numeros

---
+++ Excercises/excersise001.py
@@ -0,0 +1,9 @@
+#Realice un programa que permita ingresar dos valores e imprimir el numero mayor
+
+valor1=int(input("Introduzca el primer valor : "))
+valor2=int(input("Introduzca el segundo valor : "))
+
+if valor1>valor2:
+    print("El mayor numero es : ",valor1)
+else:
+    print("El mayor numero es : ",valor2)
