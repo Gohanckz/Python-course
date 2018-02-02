960d608ec2ec486000ba1db266d3ccd7062fb36a
taekcoder <ing.ivan.rodrigo@gmail.com>
2018-02-02 14:51:33 -0300

    FuncionesConParametros - Datos tipo tupla

---
+++ Excercises/Excersise015.py
@@ -0,0 +1,15 @@
+def cargar_fecha():
+    dd=int(input("Ingrese numero de dia:"))
+    mm=int(input("Ingrese numero de mes:"))
+    aa=int(input("Ingrese numero de año:"))
+    tupla = (dd,mm,aa)
+    return tupla
+
+def imprimir_fecha(fecha):
+    print(fecha[0],fecha[1],fecha[2],sep="/")
+
+
+# bloque principal
+
+fecha=cargar_fecha()
+imprimir_fecha(fecha)
