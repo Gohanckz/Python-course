263c30d05df94df608134c1a3d95be183a17c3f4
taekcoder <ing.ivan.rodrigo@gmail.com>
2018-02-03 00:17:40 -0300

    Cooperacion001

---
+++ POO/cooperacion001/Funciones.py
@@ -0,0 +1,12 @@
+def numero_mayor(x1,x2):
+    if x1>x2:
+        return x1
+    else:
+        return x2
+
+
+def numero_menor(x1,x2):
+    if x1<x2:
+        return x1
+    else:
+        return x2
