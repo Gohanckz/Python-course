a53147c78495df344a6db476bc313804defc8475
taekcoder <ing.ivan.rodrigo@gmail.com>
2018-02-03 00:18:34 -0300

    Cooperacion001 - MAIN

---
+++ POO/cooperacion001/Main.py
@@ -0,0 +1,6 @@
+from Funciones import numero_menor, numero_mayor
+
+n1 = int(input("Ingrese el primer numero : "))
+n2 = int(input("Ingrese el segundo numero : "))
+print("El menor de los numeros es : ",numero_menor(n1,n2))
+print("El mayor de los numeros es : ",numero_mayor(n1,n2))
