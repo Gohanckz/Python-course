cd1278a62ca3ada1f6995d078ec17aeb7886090f
taekcoder <ing.ivan.rodrigo@gmail.com>
2018-02-05 20:04:28 -0300

    POO -  Variables de Clases

---
+++ POO/VariablesDeClases/VariablesDeClases.py
@@ -0,0 +1,39 @@
+class Cliente:
+    suspendidos=[]
+
+    def __init__(self,codigo,nombre):
+        self.codigo=codigo
+        self.nombre=nombre
+
+    def imprimir(self):
+        print("Codigo:",self.codigo)
+        print("Nombre:",self.nombre)
+        self.esta_suspendido()
+
+    def esta_suspendido(self):
+        if self.codigo in Cliente.suspendidos:
+            print("Esta suspendido")
+        else:
+            print("No esta suspendido")
+        print("_____________________________")
+
+    def suspender(self):
+        Cliente.suspendidos.append(self.codigo)
+
+
+# bloque principal
+
+cliente1=Cliente(1,"Juan")
+cliente2=Cliente(2,"Ana")
+cliente3=Cliente(3,"Diego")
+cliente4=Cliente(4,"Pedro")
+
+cliente3.suspender()
+cliente4.suspender()
+
+cliente1.imprimir()
+cliente2.imprimir()
+cliente3.imprimir()
+cliente4.imprimir()
+
+print(Cliente.suspendidos)
